bf5549192cf2aa4ebeebee0d05439b813e1ab0af
dymyw <dymayongwei@163.com>
2019-10-15 18:05:37 +0800

    test pdo extension

--- www/docker.test/index.php
+++ www/docker.test/index.php
@@ -1,3 +1,15 @@
 <?php
 
 echo 'docker.test';
+
+/**
+ * 测试 PHP 的 PDO 扩展
+ */
+$pdo    = new PDO('mysql:host=mysql;dbname=mysql', 'root', 'root', [
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+]);
+$result = $pdo->query("SELECT `User` FROM `user`")->fetchAll();
+var_dump([
+    'pdo'       => $pdo,
+    'result'    => $result,
+]);
