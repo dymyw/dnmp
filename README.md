fc1db11ca887d95bb58f943126c4f08faf569957
dymyw <dymayongwei@163.com>
2019-10-16 15:23:51 +0800

    install php extensions

--- README.md
+++ README.md
@@ -37,6 +37,15 @@ Docker + Nginx + MySQL + PHP 开发环境
 
 5、浏览器访问 `http://docker.lnmp` 或 `http://docker.test`
 
+### PHP 扩展
+
+`.env.sample` 文件中有少量的扩展安装事例，如要安装更多的扩展，编辑你的 `.env` 修改 `PHP_EXTENSIONS` 配置项
+
+    # 要安装的扩展列表，用英文逗号隔开
+    PHP_EXTENSIONS=pdo_mysql,mysqli,mbstring,gd,curl,redis
+
+然后重新构建 `php` 镜像 `docker-compose build php` 启动服务 `docker-compose up -d`。可用的扩展列表请查看 `.env.sample` 的注释块说明
+
 ### 开发说明
 
 Compose 文件格式: 3.7
@@ -48,10 +57,13 @@ Compose 文件格式: 3.7
 - [ ] 开源许可
 - [ ] Nginx 支持 HTTPS
 - [ ] Nginx 应用安装
-- [ ] 安装 PHP 扩展
+- [x] 安装 PHP 扩展
 - [ ] PHP 多版本支持
-    - [ ] 7.2
+    - [x] 7.2
     - [ ] 7.1
     - [ ] 5.6
-- [ ] MySQL 配置、数据、日志
+- [ ] MySQL 服务
+    - [x] 配置
+    - [ ] 数据
+    - [ ] 日志
 - [ ] Redis 服务
--- services/php/extensions/install.sh
+++ services/php/extensions/install.sh
@@ -13,25 +13,47 @@ echo "Work directory            : ${PWD}"
 echo "============================================"
 echo
 
-if [ -z "${PHP_EXTENSIONS##*amqp*}" ]; then
+export EXTENSIONS=",${PHP_EXTENSIONS},"
+
+if [ -z "${EXTENSIONS##*,amqp,*}" ]; then
     echo "-------- Install amqp --------"
     apt-get -y install --no-install-recommends librabbitmq-dev
     pecl install amqp-1.9.3
     docker-php-ext-enable amqp
 fi
 
-if [ -z "${PHP_EXTENSIONS##*bz2*}" ]; then
+if [ -z "${EXTENSIONS##*,bcmath,*}" ]; then
+    echo "-------- Install bcmath --------"
+    docker-php-ext-install ${MC} bcmath
+fi
+
+if [ -z "${EXTENSIONS##*,bz2,*}" ]; then
     echo "-------- Install bz2 --------"
     apt-get -y install --no-install-recommends libbz2-dev
     docker-php-ext-install ${MC} bz2
 fi
 
-if [ -z "${PHP_EXTENSIONS##*exif*}" ]; then
+if [ -z "${EXTENSIONS##*,calendar,*}" ]; then
+    echo "-------- Install calendar --------"
+    docker-php-ext-install ${MC} calendar
+fi
+
+if [ -z "${EXTENSIONS##*,curl,*}" ]; then
+    echo "-------- curl is installed --------"
+fi
+
+if [ -z "${EXTENSIONS##*,enchant,*}" ]; then
+    echo "-------- Install enchant --------"
+    apt-get -y install --no-install-recommends libenchant-dev
+    docker-php-ext-install ${MC} enchant
+fi
+
+if [ -z "${EXTENSIONS##*,exif,*}" ]; then
     echo "-------- Install exif --------"
     docker-php-ext-install ${MC} exif
 fi
 
-if [ -z "${PHP_EXTENSIONS##*gd*}" ]; then
+if [ -z "${EXTENSIONS##*,gd,*}" ]; then
     echo "-------- Install gd --------"
     apt-get -y install --no-install-recommends \
         libfreetype6-dev libjpeg62-turbo-dev libpng-dev
@@ -40,19 +62,25 @@ if [ -z "${PHP_EXTENSIONS##*gd*}" ]; then
     docker-php-ext-install ${MC} gd
 fi
 
-if [ -z "${PHP_EXTENSIONS##*gettext*}" ]; then
+if [ -z "${EXTENSIONS##*,gettext,*}" ]; then
     echo "-------- Install gettext --------"
     docker-php-ext-install ${MC} gettext
 fi
 
-if [ -z "${PHP_EXTENSIONS##*imagick*}" ]; then
+if [ -z "${EXTENSIONS##*,gmp,*}" ]; then
+    echo "-------- Install gmp --------"
+    apt-get -y install --no-install-recommends libgmp-dev
+    docker-php-ext-install ${MC} gmp
+fi
+
+if [ -z "${EXTENSIONS##*,imagick,*}" ]; then
     echo "-------- Install imagick --------"
     apt-get -y install --no-install-recommends libmagickwand-dev
     pecl install imagick-3.4.4
     docker-php-ext-enable imagick
 fi
 
-if [ -z "${PHP_EXTENSIONS##*imap*}" ]; then
+if [ -z "${EXTENSIONS##*,imap,*}" ]; then
     echo "-------- Install imap --------"
     apt-get -y install --no-install-recommends \
         libc-client-dev libkrb5-dev
@@ -61,11 +89,37 @@ if [ -z "${PHP_EXTENSIONS##*imap*}" ]; then
     docker-php-ext-install ${MC} imap
 fi
 
+if [ -z "${EXTENSIONS##*,interbase,*}" ]; then
+    echo "-------- Install interbase --------"
+    apt-get -y install --no-install-recommends \
+        firebird-dev libib-util
+    docker-php-ext-install ${MC} interbase
+fi
+
+if [ -z "${EXTENSIONS##*,intl,*}" ]; then
+    echo "-------- Install intl --------"
+    apt-get -y install --no-install-recommends libicu-dev
+    docker-php-ext-install ${MC} intl
+fi
+
+if [ -z "${EXTENSIONS##*,ldap,*}" ]; then
+    echo "-------- Install ldap --------"
+    apt-get -y install --no-install-recommends libldap2-dev
+    docker-php-ext-install ${MC} ldap
+fi
+
+if [ -z "${EXTENSIONS##*,mbstring,*}" ]; then
+    echo "-------- mbstring is installed --------"
+fi
+
 # mcrypt has been deprecated in 7.1 and removed in 7.2 in favor of OpenSSL
 #   https://secure.php.net/manual/en/migration71.deprecated.php
 #   https://lukasmestan.com/install-mcrypt-extension-in-php7-2/
+if [ -z "${EXTENSIONS##*,mcrypt,*}" ]; then
+    echo "-------- mcrypt is installed --------"
+fi
 
-if [ -z "${PHP_EXTENSIONS##*memcached*}" ]; then
+if [ -z "${EXTENSIONS##*,memcached,*}" ]; then
     echo "-------- Install memcached --------"
     apt-get -y install --no-install-recommends \
         libmemcached-dev zlib1g-dev
@@ -73,52 +127,196 @@ if [ -z "${PHP_EXTENSIONS##*memcached*}" ]; then
     docker-php-ext-enable memcached
 fi
 
-if [ -z "${PHP_EXTENSIONS##*mongodb*}" ]; then
+if [ -z "${EXTENSIONS##*,mongodb,*}" ]; then
     echo "-------- Install mongodb --------"
     pecl install mongodb-1.5.3
     docker-php-ext-enable mongodb
 fi
 
-if [ -z "${PHP_EXTENSIONS##*mysqli*}" ]; then
+if [ -z "${EXTENSIONS##*,msgpack,*}" ]; then
+    echo "-------- Install msgpack --------"
+    pecl install msgpack-2.0.3
+    docker-php-ext-enable msgpack
+fi
+
+if [ -z "${EXTENSIONS##*,mysqli,*}" ]; then
     echo "-------- Install mysqli --------"
     docker-php-ext-install ${MC} mysqli
 fi
 
-if [ -z "${PHP_EXTENSIONS##*pdo_mysql*}" ]; then
+if [ -z "${EXTENSIONS##*,opcache,*}" ]; then
+    echo "-------- Install opcache --------"
+    docker-php-ext-install ${MC} opcache
+fi
+
+if [ -z "${EXTENSIONS##*,pcntl,*}" ]; then
+    echo "-------- Install pcntl --------"
+    docker-php-ext-install ${MC} pcntl
+fi
+
+if [ -z "${EXTENSIONS##*,pdo_mysql,*}" ]; then
     echo "-------- Install pdo_mysql --------"
     docker-php-ext-install ${MC} pdo_mysql
 fi
 
-if [ -z "${PHP_EXTENSIONS##*redis*}" ]; then
+if [ -z "${EXTENSIONS##*,pdo_odbc,*}" ]; then
+    echo "-------- Install pdo_odbc --------"
+    apt-get -y install --no-install-recommends unixodbc-dev
+    docker-php-ext-configure \
+        pdo_odbc --with-pdo-odbc=unixODBC,/usr
+    docker-php-ext-install ${MC} pdo_odbc
+fi
+
+if [ -z "${EXTENSIONS##*,pdo_pgsql,*}" ]; then
+    echo "-------- Install pdo_pgsql --------"
+    apt-get -y install --no-install-recommends libpq-dev
+    docker-php-ext-install ${MC} pdo_pgsql
+fi
+
+if [ -z "${EXTENSIONS##*,pdo_sqlsrv,*}" ]; then
+    echo "-------- Install pdo_sqlsrv --------"
+    apt-get -y install --no-install-recommends unixodbc-dev
+    pecl install pdo_sqlsrv-5.6.1
+    docker-php-ext-enable pdo_sqlsrv
+fi
+
+if [ -z "${EXTENSIONS##*,pgsql,*}" ]; then
+    echo "-------- Install pgsql --------"
+    apt-get -y install --no-install-recommends libpq-dev
+    docker-php-ext-install ${MC} pgsql
+fi
+
+if [ -z "${EXTENSIONS##*,pspell,*}" ]; then
+    echo "-------- Install pspell --------"
+    apt-get -y install --no-install-recommends libpspell-dev
+    docker-php-ext-install ${MC} pspell
+fi
+
+if [ -z "${EXTENSIONS##*,readline,*}" ]; then
+    echo "-------- curl is readline --------"
+fi
+
+if [ -z "${EXTENSIONS##*,recode,*}" ]; then
+    echo "-------- Install recode --------"
+    apt-get -y install --no-install-recommends librecode-dev
+    docker-php-ext-install ${MC} recode
+fi
+
+if [ -z "${EXTENSIONS##*,redis,*}" ]; then
     echo "-------- Install redis --------"
     pecl install redis-5.0.0
     docker-php-ext-enable redis
 fi
 
-if [ -z "${PHP_EXTENSIONS##*sockets*}" ]; then
+if [ -z "${EXTENSIONS##*,shmop,*}" ]; then
+    echo "-------- Install shmop --------"
+    docker-php-ext-install ${MC} shmop
+fi
+
+if [ -z "${EXTENSIONS##*,snmp,*}" ]; then
+    echo "-------- Install snmp --------"
+    apt-get -y install --no-install-recommends libsnmp-dev
+    docker-php-ext-install ${MC} snmp
+fi
+
+if [ -z "${EXTENSIONS##*,soap,*}" ]; then
+    echo "-------- Install soap --------"
+    apt-get -y install --no-install-recommends libxml2-dev
+    docker-php-ext-install ${MC} soap
+fi
+
+if [ -z "${EXTENSIONS##*,sockets,*}" ]; then
     echo "-------- Install sockets --------"
     docker-php-ext-install ${MC} sockets
 fi
 
-if [ -z "${PHP_EXTENSIONS##*xdebug*}" ]; then
+if [ -z "${EXTENSIONS##*,swoole,*}" ]; then
+    echo "-------- Install swoole --------"
+    pecl install swoole-4.4.8
+    docker-php-ext-enable swoole
+fi
+
+if [ -z "${EXTENSIONS##*,sysvmsg,*}" ]; then
+    echo "-------- Install sysvmsg --------"
+    docker-php-ext-install ${MC} sysvmsg
+fi
+
+if [ -z "${EXTENSIONS##*,sysvsem,*}" ]; then
+    echo "-------- Install sysvsem --------"
+    docker-php-ext-install ${MC} sysvsem
+fi
+
+if [ -z "${EXTENSIONS##*,sysvshm,*}" ]; then
+    echo "-------- Install sysvshm --------"
+    docker-php-ext-install ${MC} sysvshm
+fi
+
+if [ -z "${EXTENSIONS##*,tidy,*}" ]; then
+    echo "-------- Install tidy --------"
+    apt-get -y install --no-install-recommends libtidy-dev
+    docker-php-ext-install ${MC} tidy
+fi
+
+if [ -z "${EXTENSIONS##*,wddx,*}" ]; then
+    echo "-------- Install wddx --------"
+    apt-get -y install --no-install-recommends libxml2-dev
+    docker-php-ext-install ${MC} wddx
+fi
+
+if [ -z "${EXTENSIONS##*,xmlrpc,*}" ]; then
+    echo "-------- Install xmlrpc --------"
+    apt-get -y install --no-install-recommends libxml2-dev
+    docker-php-ext-install ${MC} xmlrpc
+fi
+
+if [ -z "${EXTENSIONS##*,xdebug,*}" ]; then
     echo "-------- Install xdebug --------"
     pecl install xdebug-2.6.0
     docker-php-ext-enable xdebug
     cp ./conf.d/xdebug.ini /usr/local/etc/php/conf.d/
 fi
 
-if [ -z "${PHP_EXTENSIONS##*xhprof*}" ]; then
+if [ -z "${EXTENSIONS##*,xhprof,*}" ]; then
     echo "-------- Install xhprof --------"
     mkdir xhprof-2.1.0
     tar -xzf xhprof-2.1.0.tar.gz -C xhprof-2.1.0 --strip-components=1
-    ( cd xhprof-2.1.0/extension && phpize && ./configure && make && make install )
+    ( cd xhprof-2.1.0/extension && phpize && ./configure && make ${MC} && make install )
     docker-php-ext-enable xhprof
     cp ./conf.d/xhprof.ini /usr/local/etc/php/conf.d/
 fi
 
-if [ -z "${PHP_EXTENSIONS##*yaf*}" ]; then
+if [ -z "${EXTENSIONS##*,xsl,*}" ]; then
+    echo "-------- Install xsl --------"
+    apt-get -y install --no-install-recommends libxslt-dev
+    docker-php-ext-install ${MC} xsl
+fi
+
+if [ -z "${EXTENSIONS##*,yac,*}" ]; then
+    echo "-------- Install yac --------"
+    pecl install yac-2.0.2
+    docker-php-ext-enable yac
+fi
+
+if [ -z "${EXTENSIONS##*,yaconf,*}" ]; then
+    echo "-------- Install yaconf --------"
+    pecl install yaconf-1.0.7
+    docker-php-ext-enable yaconf
+fi
+
+if [ -z "${EXTENSIONS##*,yaf,*}" ]; then
     echo "-------- Install yaf --------"
     pecl install yaf-3.0.8
     docker-php-ext-enable yaf
     cp ./conf.d/yaf.ini /usr/local/etc/php/conf.d/
+fi
+
+if [ -z "${EXTENSIONS##*,zend_test,*}" ]; then
+    echo "-------- Install zend_test --------"
+    docker-php-ext-install ${MC} zend_test
+fi
+
+if [ -z "${EXTENSIONS##*,zip,*}" ]; then
+    echo "-------- Install zip --------"
+    apt-get -y install --no-install-recommends libzip-dev
+    docker-php-ext-install ${MC} zip
 fi
